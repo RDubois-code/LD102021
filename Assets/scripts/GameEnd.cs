4cc01024616b87163dd6e00fbd9cdfe1385ab721
johandu <johan.dumestier@outlook.fr>
2021-10-02 18:04:40 +0200

    new level

--- Assets/scripts/GameEnd.cs
+++ Assets/scripts/GameEnd.cs
@@ -9,7 +9,11 @@ public class GameEnd : MonoBehaviour
     public Transform head;
     public Transform mainCamera;
     public GameObject victoryScreen;
+    public GameObject gameOverScreen;
+    public string nextScene;
+
     bool victory = false;
+    bool gameOver = false;
 
     void FixedUpdate()
     {
@@ -18,18 +22,36 @@ public class GameEnd : MonoBehaviour
             player.position = head.position;
             player.GetComponent<Rigidbody2D>().Sleep();
         }
-        if(victory && Input.anyKeyDown)
+
+        if(victory && Input.anyKey)
         {
             int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
-            if (SceneManager.sceneCount > nextSceneIndex)
+
+            if(nextScene != null)
             {
-                SceneManager.LoadScene(nextSceneIndex);
+                SceneManager.LoadScene(nextScene);
             }
             else
             {
                 SceneManager.LoadScene(0);
             }
         }
+
+        if (gameOver && Input.anyKey)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);            
+        }
+
+        if (player.gameObject.GetComponent<PlayerBehaviour>().scale < 0.5f)
+        {
+            gameOver = true;
+            player.GetComponent<Rigidbody2D>().Sleep();
+            Vector3 vector = new Vector3(mainCamera.position.x, mainCamera.position.y, this.transform.position.z - 3);
+            GameObject.Instantiate(gameOverScreen, vector, Quaternion.identity);
+
+
+        }
+
     } 
 
     void OnTriggerEnter2D(Collider2D col)
