8b82509808c63220e64800f82ff8901629c0f10e
Xyrion <RemiEric.Dubois@Gmail.com>
2021-10-02 16:47:20 +0200

    Add basic melting handling to PlayerBehaviour.

--- Assets/scripts/PlayerBehaviour.cs
+++ Assets/scripts/PlayerBehaviour.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    public float moveForce = 10.0f;
+    public float groundControl = 10.0f;
+    public float airControl = 5.0f;
+
     public float jumpForce = 100.0f;
+
+    public float grassMeltingFactor = 0.1f;
+    public float snowGrowingFactor = 0.1f;
+
+    public float scale = 1.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,12 +23,41 @@ public class PlayerBehaviour : MonoBehaviour
     // FixedUpdate is called at a fixed rate
     void FixedUpdate()
     {
-        this.myRigidbody.AddForce(Vector3.right * Input.GetAxis("Horizontal") * this.moveForce);
+        HandleMovement();
 
-        if(Input.GetButton("Jump") && this.myRigidbody.IsTouchingLayers(Physics2D.AllLayers))
-		{
-            this.myRigidbody.AddForce(Vector3.up * this.jumpForce);
+        HandleScaling();
+    }
+
+    void HandleMovement()
+    {
+        if(this.myRigidbody.IsTouchingLayers(LayerMask.GetMask("Grass", "Snow")))
+        {
+            this.myRigidbody.AddForce(Vector3.right * Input.GetAxis("Horizontal") * this.groundControl);
+
+            if (Input.GetButton("Jump"))
+            {
+                this.myRigidbody.AddForce(Vector3.up * this.jumpForce);
+            }
         }
+        else
+		{
+            this.myRigidbody.AddForce(Vector3.right * Input.GetAxis("Horizontal") * this.airControl);
+        }
+    }
+
+    void HandleScaling()
+    {
+        if (this.myRigidbody.IsTouchingLayers(LayerMask.GetMask("Grass")))
+        {
+            this.scale -= this.grassMeltingFactor * Time.deltaTime;
+        }
+        else if (this.myRigidbody.IsTouchingLayers(LayerMask.GetMask("Snow")))
+        {
+            this.scale += this.snowGrowingFactor * Time.deltaTime;
+        }
+
+        this.transform.localScale = Vector3.one * this.scale;
+        this.myRigidbody.mass = this.scale;
     }
 
     Rigidbody2D myRigidbody;
